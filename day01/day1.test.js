8d490b8c35ae79c4db251a94c03caeac50546316
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-12-03 10:57:32 -0500

    Switch to use chai

--- day01/day1.test.js
+++ day01/day1.test.js
@@ -1,28 +1,28 @@
-const { equal } = require('assert');
+const { assert } = require('chai');
 const { fuelRequired, fuelRequiredWithFuelMass } = require('./day1.js');
 const input = require('./input.js');
 
 module.exports = {
   question1_samples() {
-    equal(fuelRequired(12), 2);
-    equal(fuelRequired(14), 2);
-    equal(fuelRequired(1969), 654);
-    equal(fuelRequired(100756), 33583);
+    assert.equal(fuelRequired(12), 2);
+    assert.equal(fuelRequired(14), 2);
+    assert.equal(fuelRequired(1969), 654);
+    assert.equal(fuelRequired(100756), 33583);
   },
 
   question1_answer() {
     const answer = input.reduce((acc, i) => acc + fuelRequired(i), 0);
-    equal(answer, 3226488);
+    assert.equal(answer, 3226488);
   },
 
   question2_samples() {
-    equal(fuelRequiredWithFuelMass(14), 2);
-    equal(fuelRequiredWithFuelMass(1969), 966);
-    equal(fuelRequiredWithFuelMass(100756), 50346);
+    assert.equal(fuelRequiredWithFuelMass(14), 2);
+    assert.equal(fuelRequiredWithFuelMass(1969), 966);
+    assert.equal(fuelRequiredWithFuelMass(100756), 50346);
   },
 
   question2_answer() {
     const answer = input.reduce((acc, i) => acc + fuelRequiredWithFuelMass(i), 0);
-    equal(answer, 4836845);
+    assert.equal(answer, 4836845);
   },
 };
