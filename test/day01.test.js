88c65a1bd166386ce394daa3cfa244ab19312f01
Vadim Brodsky <vadim.brodsky@gmail.com>
2019-12-03 11:19:54 -0500

    Better test case naming

--- test/day01.test.js
+++ test/day01.test.js
@@ -3,25 +3,25 @@ const { fuelRequired, fuelRequiredWithFuelMass } = require('../src/day01.js');
 const input = require('./input/day01-input.js');
 
 module.exports = {
-  question1_samples() {
+  day01_part1_samples() {
     assert.equal(fuelRequired(12), 2);
     assert.equal(fuelRequired(14), 2);
     assert.equal(fuelRequired(1969), 654);
     assert.equal(fuelRequired(100756), 33583);
   },
 
-  question1_answer() {
+  day01_part1_answer() {
     const answer = input.reduce((acc, i) => acc + fuelRequired(i), 0);
     assert.equal(answer, 3226488);
   },
 
-  question2_samples() {
+  day01_part2_samples() {
     assert.equal(fuelRequiredWithFuelMass(14), 2);
     assert.equal(fuelRequiredWithFuelMass(1969), 966);
     assert.equal(fuelRequiredWithFuelMass(100756), 50346);
   },
 
-  question2_answer() {
+  day01_part2_answer() {
     const answer = input.reduce((acc, i) => acc + fuelRequiredWithFuelMass(i), 0);
     assert.equal(answer, 4836845);
   },
--- test/day02.test.js
+++ test/day02.test.js
@@ -3,14 +3,14 @@ const { optCodeComputer } = require('../src/day02.js');
 const input = require('./input/day02-input.js');
 
 module.exports = {
-  question2_samples() {
+  day02_part1_samples() {
     assert.deepEqual(optCodeComputer([1, 0, 0, 0, 99]), [2, 0, 0, 0, 99]);
     assert.deepEqual(optCodeComputer([2, 3, 0, 3, 99]), [2, 3, 0, 6, 99]);
     assert.deepEqual(optCodeComputer([2, 4, 4, 5, 99, 0]), [2, 4, 4, 5, 99, 9801]);
     assert.deepEqual(optCodeComputer([1, 1, 1, 4, 99, 5, 6, 0, 99]), [30, 1, 1, 4, 2, 5, 6, 0, 99]);
   },
 
-  question2_answer() {
+  day02_part1_answer() {
     input[1] = 12;
     input[2] = 2;
     assert.equal(optCodeComputer(input)[0], 5098658);
